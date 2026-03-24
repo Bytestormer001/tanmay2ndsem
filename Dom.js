f4678bb7aa13e1eacc0ab9c560d1305e4f37756d
Bytestormer001 <tanmay.25b01010606@abes.ac.in>
2026-03-24 22:01:18 +0530

    Add files via upload

---
+++ Dom.js
@@ -0,0 +1,14 @@
+//document.writeIN("hello");
+
+function result(){
+    let changeColor=document.getElementById('text');
+    changeColor.style.color="red";
+
+    let bodycolor=document.getElementsByTagName('body')[0];
+    bodycolor.style.backgroundColor="pink";
+}
+let para=document.grtElementByID("paratext");
+function ChangeTextColor(){
+    para.style.backgroundColor="#"+Math 
+    }
+}
